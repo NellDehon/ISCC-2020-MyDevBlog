4b542e0e9466dac62cf00ffae09fab9c3715add6
NellDehon <nelldehon@icloud.com>
2020-07-01 17:43:11 +0200

    ISCC-2020-MyDevBlog

---
+++ front.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="back.css" >
+</head>
+<header>
+<h1> MyDevBlog</h1> 
+<nav>
+            <a class="navigationcolor" href="accueil.html" >Accueil</a>
+            <a class="navigationcolor" href="contact.html"> Contact</a> 
+            <a class="navigationcolor" href="articles.html"> Articles </a>
+        </nav> 
+
+
+</header>
+<p> Ceci est une courte description du projet </p>
+</body>
+</html>
